cf03a664b040db1e02644dc88d9bffb03f9959f6
Pham Bao Long <pblo@netcompany.com>
2023-03-22 14:32:56 +0700

    added GET user, returning mock-users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { mockUsers, User } from './user';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  getUser(): Array<User> {
+    return mockUsers;
   }
 }
