21f94609eda8ada975a85cede9b51ca5d3e452c7
Pham Bao Long <pblo@netcompany.com>
2023-03-23 17:00:35 +0700

    mmmmm

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbContext } from './db/db-context';
 
 describe('AppController', () => {
   let appController: AppController;
-
+  process.env.COSMOS_DB_ENDPOINT = 'http://localhost:8080';
+  process.env.COSMOS_DB_KEY = 'asdasd';
+  process.env.COSMOS_DB_ID = 'asdasd';
+  
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, DbContext],
     }).compile();
 
     appController = app.get<AppController>(AppController);
