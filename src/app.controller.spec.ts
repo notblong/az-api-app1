08a6bc7d9773f9b84ee2670115198920f23f986f
Pham Bao Long <pblo@netcompany.com>
2023-03-30 16:02:44 +0700

    experiment: speed up deployment

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,29 +1,4 @@
-import { HttpModule } from '@nestjs/axios';
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BlobContext } from './blob/blob-context';
-import { DbContext } from './db/db-context';
-import { DocumentService } from './document/document.service';
-import { ServiceBus } from './queue/service-bus';
-
 describe('AppController', () => {
-  // let appController: AppController;
-  // process.env.COSMOS_DB_ENDPOINT = 'http://localhost:8080';
-  // process.env.COSMOS_DB_KEY = 'asdasd';
-  // process.env.COSMOS_DB_ID = 'asdasd';
-  // process.env.AZURE_STORAGE_CONNECTION_STRING = 'lskjadhfkjhaslkdfhkj';
-  
-  beforeEach(async () => {
-    // const app: TestingModule = await Test.createTestingModule({
-    //   imports: [HttpModule, ServiceBus],
-    //   controllers: [AppController],
-    //   providers: [AppService, DbContext, BlobContext, ServiceBus, DocumentService],
-    // }).compile();
-
-    // appController = app.get<AppController>(AppController);
-  });
-
   describe('root', () => {
     it('should pass"', () => {
       expect(true).toBe(true);
